db25dfe073abc820f143ec5305ce5fa5c9354ce1
lenkazavadil321 <lenkazavadil321@gmail.com>
2025-03-10 00:14:37 +0000

    Add 6.ts

---
+++ src/6.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'SchoolProject';
+}
