a140eb9aabf1741a0e23652b040fa1bb747047d6
lenkazavadil321 <lenkazavadil321@gmail.com>
2025-03-20 01:13:24 +0000

    Add 15.ts

---
+++ src/15.ts
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={handleClick}>Increment</button>
+    </div>
+  );
+}
